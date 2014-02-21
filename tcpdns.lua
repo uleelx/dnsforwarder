5df4fe5a59d21d861c493759ab248677d8f43db9
Peer Zeng <uleelx@foxmail.com>
2014-02-21 14:31:28 +0800

    add LRU cache

--- tcpdns.lua
+++ tcpdns.lua
@@ -1,10 +1,40 @@
 local socket = socket or require("socket")
 local struct = struct or require("struct")
+local insert, remove = table.insert, table.remove
 
 local hosts = {"8.8.8.8", "8.8.4.4", "208.67.222.222", "208.67.220.220"}
 local udp = socket.udp()
 local threads = {}
-local mem = {}
+
+local function LRU(size)
+  local keys, dic, lru = {}, {}, {}
+
+  function lru.add(key, value)
+    if #keys == size then
+      dic[keys[size]] = nil
+      remove(keys)
+    end
+    insert(keys, 1, key)
+    dic[key] = value
+  end
+
+  function lru.get(key)
+    local value = dic[key]
+    if value and keys[1] ~= key then
+      for i, k in ipairs(keys) do
+        if k == key then
+          insert(keys, 1, remove(keys, i))
+          break
+        end
+      end
+    end
+    return value
+  end
+
+  return lru
+end
+
+local cache = LRU(20)
 
 local function queryDNS(host, data)
   local sock = socket.tcp()
@@ -27,8 +57,9 @@ local function new(data, ip, port)
     local data, ip, port = data, ip, port
     local domain = (data:sub(14, -6):gsub("[^%w]", "."))
     print("domain: "..domain, "thread: "..#threads)
-    if mem[domain] then
-      udp:sendto(data:sub(1, 2)..mem[domain], ip, port)
+    local packet = cache.get(domain)
+    if packet then
+      udp:sendto(data:sub(1, 2)..packet, ip, port)
     else
       for _, host in ipairs(hosts) do
         data = queryDNS(host, data)
@@ -36,7 +67,7 @@ local function new(data, ip, port)
       end
       if #data > 0 then
         data = data:sub(3)
-        mem[domain] = data:sub(3)
+        cache.add(domain, data:sub(3))
         udp:sendto(data, ip, port)
       end
     end
@@ -48,7 +79,7 @@ local function dispatch()
   while threads[i] do
     local sock = threads[i]()
     if not sock then
-      table.remove(threads, i)
+      remove(threads, i)
     else
       i = i + 1
     end
@@ -62,7 +93,7 @@ local function mainLoop()
   local data, ip, port
   while true do
     data, ip, port = udp:receivefrom()
-    if data then threads[#threads + 1] = new(data, ip, port) end
+    if data then insert(threads, new(data, ip, port)) end
     if #threads > 0 then dispatch() else socket.sleep(0.01) end
   end
 end
